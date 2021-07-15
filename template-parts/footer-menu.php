74d503b44c0316630901b30e7faf73772e5a5e76
Daniel Rincón Hdez <danielrinconher@gmail.com>
2021-07-14 20:54:23 -0500

    Link fix

--- template-parts/footer-menu.php
+++ template-parts/footer-menu.php
@@ -20,8 +20,8 @@ if ( has_nav_menu( 'footer-menu' ) ) {
     <div class="footer-menu container-xxl">
       <ul>
         <li><a href="<?php echo home_url() . '/#store' ?>" target="_blank"><?php _e( 'Store', 'cornerstore' ) ?></a></li>
-        <li><a href="<?php echo home_url() . '/contact' ?>" target="_blank"><?php _e( 'Contact', 'cornerstore' ) ?></a></li>
-        <li><a href="<?php echo home_url() . '/about' ?>" target="_blank"><?php _e( 'About', 'cornerstore' ) ?> </a></li>
+        <li><a href="<?php echo home_url() . '/contact/' ?>" target="_blank"><?php _e( 'Contact', 'cornerstore' ) ?></a></li>
+        <li><a href="<?php echo home_url() . '/about/' ?>" target="_blank"><?php _e( 'About', 'cornerstore' ) ?> </a></li>
         <li><a href="https://api.whatsapp.com/send?phone=<?php echo get_store_phone_number() ?>&text=<?php _e( 'Hello', 'cornerstore' ) ?>" target="_blank"><?php _e( 'Chat', 'cornerstore' ) ?></a></li>
         <li><a href="<?php echo get_store_location() ?>" target="_blank"><?php _e( 'Location', 'cornerstore' ) ?></a></li>
       </ul>
